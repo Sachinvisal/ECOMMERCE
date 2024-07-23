f9d8b0fc3ecec3d4b540f3088d0780f543f9878e
sachinsv+1-at-832266882009 <sachinsv@cf.lk>
2024-07-23 16:43:25 +0530

    implement the model part

--- src/main/java/com/example/ecommerce/model/Category.java
+++ src/main/java/com/example/ecommerce/model/Category.java
@@ -1,4 +1,13 @@
 package com.example.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Category {
 }
