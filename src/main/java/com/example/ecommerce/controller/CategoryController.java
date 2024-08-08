07ae4df9179d3948cbd1a94cf0247a9dc0e5a80e
sachinsv+1-at-832266882009 <sachinsv@cf.lk>
2024-08-08 16:09:00 +0530

    implemented the Category controller and Category Services

--- src/main/java/com/example/ecommerce/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce/controller/CategoryController.java
@@ -1,4 +1,47 @@
 package com.example.ecommerce.controller;
 
+
+import com.example.ecommerce.dto.CategoryDTO;
+import com.example.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/Category")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+
+    //Order Show
+    @GetMapping("/getCategory")
+    public List<CategoryDTO> getCategory(){
+        return categoryService.getCategoryAll();
+    }
+
+    //Save part
+    @PostMapping("/addCategory")
+    public CategoryDTO addCategory(@RequestBody CategoryDTO categoryDTO){
+        return  categoryService.addCategory(categoryDTO);
+    }
+
+    //Order Delete
+    @DeleteMapping("/deleteCategory/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+    }
+
+    //Order Update
+    @PutMapping("/categoryUpdate/{id}")
+    public CategoryDTO categoryUpdate(@RequestBody CategoryDTO categoryDTO, @PathVariable Long id){
+        try{
+            return categoryService.updateCategory(categoryDTO,id);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
 }
--- src/main/java/com/example/ecommerce/service/CategoryService.java
+++ src/main/java/com/example/ecommerce/service/CategoryService.java
@@ -1,4 +1,51 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.dto.CategoryDTO;
+import com.example.ecommerce.model.Category;
+import com.example.ecommerce.repo.CategoryRepo;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final ModelMapper modelMapper;
+    private final CategoryRepo categoryRepo;
+
+
+    // (get all Category show in this function)
+    public List<CategoryDTO> getCategoryAll(){
+        List<Category> userList = categoryRepo.findAll();
+        return modelMapper.map(userList,new TypeToken<List<CategoryDTO>>(){}.getType());
+    }
+
+    //(Add Category method )
+    @Transactional
+    public CategoryDTO addCategory(CategoryDTO categoryDTO){
+        categoryRepo.save(modelMapper.map(categoryDTO,Category.class));
+        return categoryDTO;
+    }
+
+    //Delete Category
+    public void deleteCategory(long userID){
+        categoryRepo.deleteById(userID);
+    }
+
+
+    //Update User
+    @Transactional
+    public CategoryDTO updateCategory(CategoryDTO categoryDTO, Long id){
+        Category existingCategory = categoryRepo.findById(id).orElseThrow(()-> new EntityNotFoundException("User Not Found WIth Id" + id));
+        existingCategory.setCatDes(categoryDTO.getCat_des());
+        existingCategory.setCatName(categoryDTO.getCat_name());
+        return categoryDTO;
+
+    }
+
 }
